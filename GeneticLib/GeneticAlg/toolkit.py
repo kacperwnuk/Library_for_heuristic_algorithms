fc509cf7592133567bff2ff9fd6c0fd043ebe556
KacperWnuk <kacperwnuk97@o2.pl>
2019-03-06 20:15:54 +0100

    calculate_fitness_values: change function to allow passing chromosome instead of attributes of chromosome - 2 options now
    select_roulette: added parameter which determines if there will be replacements in output, changed conception - used numpy lib
    added function select_linear, create_couples
    added representation of an individual

--- GeneticLib/GeneticAlg/toolkit.py
+++ GeneticLib/GeneticAlg/toolkit.py
@@ -1,4 +1,5 @@
 import random
+import numpy.random
 
 
 class Toolkit:
@@ -43,9 +44,10 @@ class Toolkit:
         else:
             raise TypeError('Chromosomes should be passed in list!')
 
-    def calculate_fitness_values(self, individuals, list_of_attributes, list_of_funcs):
+    def calculate_fitness_values(self, individuals, list_of_funcs, list_of_attributes=None):
         """
-            Modifying existing population, not creating new one
+            Modifying existing population, not creating new one.
+            If there is no list_of_attributes then chromosome is passed to function.
             Attributes:
                 individuals: list of individuals
                 list_of_attributes: list of attributes used to calculate fitness value
@@ -59,18 +61,21 @@ class Toolkit:
             raise TypeError('Individuals should be stored in a list!')
         if type(list_of_funcs) is not list:
             raise TypeError('Fitness functions should be stored in a list!')
-        if type(list_of_attributes) is not list:
+        if list_of_attributes is not None and type(list_of_attributes) is not list:
             raise TypeError('Attributes should be stored in list!')
         if len(list_of_funcs) != len(self.weights):
             raise ValueError('Amount of functions should be equal amount of weights!')
 
-        if len(list_of_attributes) < len(self.weights):
+        if list_of_attributes is not None and len(list_of_attributes) < len(self.weights):
             list_of_attributes += [list_of_attributes[-1] for _ in
                                    range(0, len(self.weights) - len(list_of_attributes))]
-
-        for individual in individuals:
-            individual.values = tuple(func(getattr(individual.chromosome, attribute)) for func, attribute in
-                                      zip(list_of_funcs, list_of_attributes))
+        if list_of_attributes is None:
+            for individual in individuals:
+                individual.values = tuple(func(individual.chromosome) for func in list_of_funcs)
+        else:
+            for individual in individuals:
+                individual.values = tuple(func(getattr(individual.chromosome, attribute)) for func, attribute in
+                                          zip(list_of_funcs, list_of_attributes))
 
     @staticmethod
     def select_random(individuals, k):
@@ -120,51 +125,92 @@ class Toolkit:
 
         return sorted(individuals, key=lambda x: x.values[key], reverse=should_reverse)[:k]
 
-    def select_roulette(self, individuals, k, key=0):
+    def select_roulette(self, individuals, k, key=0, replacement=False):
         """
             Picks k individuals using roulette method
             https://en.wikipedia.org/wiki/Fitness_proportionate_selection
+            It is possible that in a return list there will be duplications of individuals!
+            Works only with maximising problem!
+            Attributes:
+                individuals: list of individuals
+                k: amount of individuals to be picked
+                key: determines which fitness value should be used
+                replacement: determines if individual can be chosen more than once
+            Locals:
+                sum_fitness: sum of all fitness values considering key
+                probabilities: list of probabilities of individuals
+            Returns:
+                list of picked individuals
+        """
+        if self.weights[key] < 0:
+            raise ValueError('Roulette selection works only with maximising problem!')
+
+        sum_fitness = sum(ind.values[key] for ind in individuals)
+
+        if self.weights[key] >= 0:
+            probabilities = [ind.values[key] / sum_fitness for ind in individuals]
+
+        return numpy.random.choice(individuals, k, replace=replacement, p=probabilities)
+
+    def select_linear(self, individuals, k, key=0, replacement=False):
+        """
+            Picks k individuals using linear rank selection
             It is possible that in a return list there will be duplications of individuals!
             Attributes:
                 individuals: list of individuals
                 k: amount of individuals to be picked
                 key: determines which fitness value should be used
+                replacement: determines if individual can be chosen more than once
             Locals:
-                should_reverse: determines if function is minimizing or maximizing fitness value
+                probabilities: list of probabilities of individuals
             Returns:
                 list of picked individuals
         """
-        chosen = []
-
         if self.weights[key] >= 0:
-            inds = sorted(individuals, key=lambda x: x.values[key], reverse=True)
-            sum_values = sum(ind.values[key] for ind in inds)
-            for _ in range(0, k):
-                rand_choice = random.randrange(1, sum_values + 1)
-                _sum = 0
-                for ind in inds:
-                    _sum += ind.values[key]
-                    if _sum >= rand_choice:
-                        chosen.append(ind)
-                        print(_sum)
-                        break
+            sorted_individuals = sorted(individuals, reverse=True, key=lambda x: x.values[key])
         else:
-            inds = sorted(individuals, key=lambda x: x.values[key], reverse=False)
-            """ values used in roulette f = fmax - f"""
-            values = [inds[-1].values[key] - ind.values[key] for ind in inds]
-            sum_values = sum(values)
-            for _ in range(0, k):
-                rand_choice = random.randrange(1, sum_values + 1)
-                _sum = 0
+            sorted_individuals = sorted(individuals, key=lambda x: x.values[key])
+        n = len(sorted_individuals)
+        _sum = (1 + n) * n / 2
+        probabilities = [i / _sum for i in range(n, 0, -1)]
 
-                for i in range(0, len(inds)):
-                    _sum += values[i]
-                    if _sum >= rand_choice:
-                        chosen.append(inds[i])
-                        print(_sum)
-                        break
+        return numpy.random.choice(sorted_individuals, k, replace=replacement, p=probabilities)
 
-        return chosen
+    @staticmethod
+    def create_couples(individuals, size, length, key=None, select_function=None, replacement=False):
+        """
+            Creates list of tuples where one tuple is a 'couple'. If key is None then algorithm packs
+            individuals in tuples in order they are stored in a list. If key is chosen then individuals are
+            picked using method passed in 'select_function' algorithm.
+            Attributes:
+                individuals: list of individuals
+                size: size of a 'couple'
+                length: amount of 'couples'
+                replacement: determines if individual can be chosen more than once
+                key: determines which fitness value should be used
+                select_function: function used in selection of individuals
+            Locals:
+
+            Returns:
+                list of couples [(1,2,3..),()..]
+        """
+        if key is not None and select_function is None:
+            raise ValueError('You need to pass function which will be used in selection!')
+
+        if not replacement or key is None:
+            if size * length > len(individuals):
+                raise ValueError('There is not enough individuals to choose without replacement!')
+
+        if key is None:
+            couples = [tuple(individuals[begin:begin + size]) for begin in range(0, length * size, size)]
+        elif replacement:
+            list_with_replacements = select_function(individuals, size * length, key=key, replacement=True)
+            couples = [tuple(list_with_replacements[begin:begin + size]) for begin in range(0, length * size, size)]
+        else:
+            list_without_replacements = select_function(individuals, size * length, key=key)
+            couples = [tuple(list_without_replacements[begin:begin + size]) for begin in range(0, length * size, size)]
+
+        return couples
 
 
 class Individual:
@@ -172,3 +218,10 @@ class Individual:
     def __init__(self, chromosome=None):
         self.chromosome = chromosome
         self.values = None
+
+    def __str__(self):
+        return self.chromosome.binary
+
+    def __repr__(self):
+        return "<{} {}>".format(self.chromosome.binary, self.values[0])
+        #return "<{}>".format(self.values[0])
